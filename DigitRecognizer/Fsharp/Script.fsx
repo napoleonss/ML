439986839ae35e494eb15b44cd68d4681349f654
Napoleon Skolarikis <napoleonss@windowslive.com>
2018-03-29 16:07:42 +0200

    Fsx Ch1.

---
+++ DigitRecognizer/Fsharp/Script.fsx
@@ -0,0 +1,18 @@
+﻿open System.IO
+
+type Observation = { Label:string; Pixels:int[]}
+
+let trainingPath = @"C:\Users\nskolari\Documents\Projects\ML\DigitRecognizer\Data\trainingsample.csv"
+let validationPath = @"C:\Users\nskolari\Documents\Projects\ML\DigitRecognizer\Data\validationsample.csv"
+
+let toObservation (csvData:string) = 
+    let columns = csvData.Split(',')
+    let label = columns.[0]
+    let pixels = columns.[1..] |> Array.map int
+    { Label=label; Pixels = pixels}
+
+let reader path = 
+    let data = File.ReadAllLines path
+    data.[1..] |> Array.map toObservation
+
+let trainingData = reader trainingPath
